54d57f316c4ec46223d71a71e6c52aa1e521a50d
pri-23-03 <prirathee2003@gmail.com>
2025-11-09 05:12:19 -0500

    chore(phase2): adapters package exports

--- gp-trader/src/gptrader/adapters/__init__.py
+++ gp-trader/src/gptrader/adapters/__init__.py
@@ -1,1 +1,16 @@
-# adapters package
+from .eventbus import EventBus, LocalEventBus
+from .exec import Executor, NoopExecutor
+from .factory import make_bus, make_executor, make_index
+from .index import Index, LocalIndex
+
+__all__ = [
+    "EventBus",
+    "LocalEventBus",
+    "Index",
+    "LocalIndex",
+    "Executor",
+    "NoopExecutor",
+    "make_bus",
+    "make_index",
+    "make_executor",
+]
